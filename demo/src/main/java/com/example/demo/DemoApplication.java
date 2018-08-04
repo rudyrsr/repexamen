f7c3abb300de5492ada04641094d2b14cc2ff53c
rudyrsr <rudy3112rsr@gmail.com>
2018-08-03 22:32:46 -0400

    haciendo cherry-pick

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
+<<<<<<< HEAD
 		System.out.println("hola ");
+=======
+>>>>>>> 3e4d400... iniciando master
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
